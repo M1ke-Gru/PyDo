50f93a23ed814864fe33d97a76e9a4de725aae05
Mike Birn <2023postline@proton.me>
2024-04-12 21:23:34 +0200

    Created a window for task settings and a button to access it

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ class MainWindow(QMainWindow):
 
     def show_UI(self):
         if not self.justLaunched:
-            self.clearLayout()
+            self.clear_layout()
         self.justLaunched = False
         self.displaytodoordone = 0
         self.windowlayout = QVBoxLayout()
@@ -36,7 +36,7 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(self.windowlayout)
         self.setCentralWidget(central_widget)
         self.plusButton = QPushButton("+")
-        self.plusButton.clicked.connect(self.addTask)
+        self.plusButton.clicked.connect(self.add_task)
 
         top_bar = QHBoxLayout()
         self.choicetodoordoneshown = QComboBox()
@@ -61,34 +61,84 @@ class MainWindow(QMainWindow):
         self.tasklistwidget = QListWidget()
         self.tododonelayout.addWidget(self.tasklistwidget)
         if self.displaytodoordone < 2:
-            self.showTasksOnLists(todo)
+            self.show_tasks_on_lists(todo)
         if self.displaytodoordone % 2 == 0:
-            self.showTasksOnLists(done)
+            self.show_tasks_on_lists(done)
 
-    def clearLayout(self):
+    def clear_layout(self):
         while self.windowlayout.count() > 0:
             item = self.windowlayout.takeAt(0)
             widget = item.widget()
             if widget:
                 widget.deleteLater()
 
-    def addTask(self):
+    def add_task(self):
         task = Task()
         todo.append(task)
         print(todo)
         self.change_todo_done_visibility()
 
-    def showTasksOnLists(self, tasklist):
+    def show_tasks_on_lists(self, tasklist):
         for task in tasklist:
             item = TaskWidget(task, self.tasklistwidget, self)
             self.tasklistwidget.addItem(item)
+
+    def show_task_settings(self, task):
+        self.taskSettingsWindow = TaskSettingsWindow(task)
+        self.taskSettingsWindow.show()
+
+
+class TaskSettingsWindow(QWidget):
+    def __init__(self, task):
+        super().__init__()
+        self.task = task
+        self.tasksettingslayout = QVBoxLayout()
+        self.show_name_editor()
+        self.show_date_editor()
+        self.show_importance_and_urgency_toggles()
+        self.setLayout(self.tasksettingslayout)
+
+    def show_name_editor(self):
+        self.nameeditorlabel = QLabel("Name")
+        self.nameeditorfield = QLineEdit(self.task.text)
+        self.nameeditorlayout = QHBoxLayout()
+        self.nameeditorlayout.addWidget(self.nameeditorlabel)
+        self.nameeditorlayout.addWidget(self.nameeditorfield)
+        self.tasksettingslayout.addLayout(self.nameeditorlayout)
+    
+    def show_date_editor(self):
+        self.dateeditorlabel = QLabel("Time")
+        self.dateeditorfield = QLineEdit("dd/mm/yy")
+        self.dateeditorlayout = QHBoxLayout()
+        self.dateeditorlayout.addWidget(self.dateeditorlabel)
+        self.dateeditorlayout.addWidget(self.dateeditorfield)
+        self.tasksettingslayout.addLayout(self.dateeditorlayout)
+
+    def show_importance_and_urgency_toggles(self):
+        self.importanceeditorlabel = QLabel("Importance")
+        self.importanceeditorfield = QComboBox()
+        self.importanceeditorfield.addItems(["High", "Medium", "Low"])
+        self.urgencyeditorlabel = QLabel("Urgency")
+        self.urgencyeditorfield = QComboBox()
+        self.urgencyeditorfield.addItems(["High", "Medium", "Low"])
+        self.importanceandurgencylayout = QHBoxLayout()
+        self.importanceandurgencylayout.addWidget(self.importanceeditorlabel)
+        self.importanceandurgencylayout.addWidget(self.importanceeditorfield)
+        self.importanceandurgencylayout.addWidget(self.urgencyeditorlabel)
+        self.importanceandurgencylayout.addWidget(self.urgencyeditorfield)
+        self.tasksettingslayout.addLayout(self.importanceandurgencylayout)
 
 
 class Task:
     def __init__(self):
         self.text = ""
         self.isDone = False
-
+        self.taskDate = ""
+        self.taskPlace = ""
+        self.importanceList = ["High", "Medium", "Low"]
+        self.importance = self.importanceList[2]
+        self.urgencyList = ["High", "Medium", "Low"]
+    
 
 class TaskWidget(QListWidgetItem):
     def __init__(self, task, listwidget, window):
@@ -98,10 +148,12 @@ class TaskWidget(QListWidgetItem):
         self.window = window
 
         self.check = QCheckBox()
-        self.check.stateChanged.connect(self.taskDoneStateChange)
+        self.check.stateChanged.connect(self.task_done_state_change)
         if (self.task.isDone == False):
             self.name = QLineEdit(self.task.text)
             self.name.textChanged.connect(self.text_changed)
+            self.burgermenubutton = QPushButton("☰")
+            self.burgermenubutton.clicked.connect(lambda: self.window.show_task_settings(self.task))
         else:
             self.name = QLabel()
             self.name.setText(self.task.text)
@@ -113,6 +165,8 @@ class TaskWidget(QListWidgetItem):
         self.tasklayout.setAlignment(Qt.AlignLeft)
         self.tasklayout.addWidget(self.check)
         self.tasklayout.addWidget(self.name)
+        if isinstance(self.name, QLineEdit):
+            self.tasklayout.addWidget(self.burgermenubutton)
         self.name.setAlignment(Qt.AlignLeft)
         self.setSizeHint(widget.sizeHint())
         self.listwidget.addItem(self)
@@ -121,7 +175,7 @@ class TaskWidget(QListWidgetItem):
     def text_changed(self, text):
         self.task.text = text
 
-    def taskDoneStateChange(self, state):
+    def task_done_state_change(self, state):
         if not self.task.isDone:
             todo.remove(self.task)
             done.append(self.task)
